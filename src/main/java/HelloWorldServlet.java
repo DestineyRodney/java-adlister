ae0fed895a0abee98bf3b394898bddf6de707454
Destiney Rodney <destiney.rodney01@gmail.com>
2021-11-30 14:24:59 -0600

    feat: Completeed servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,12 +8,27 @@ import java.io.PrintWriter;
 
 @WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+    private int number = 0;
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         String searchQuery = req.getParameter("q");
-        PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World!</h1>");
 
+        String name = req.getParameter("q");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, " + name + "</h1>");
+
+        if  (name == null) {
+            out.println("Hello, World!");
+        }else if (name != null){
+            out.println("Hello, " + name + "!");
+        }
+
+//        counter
+        number++;
+        out.println(number);
+
+//        Lecture
         if(searchQuery != null) {
             out.println("<p>Here are your results for " + searchQuery + "</p>");
         }
