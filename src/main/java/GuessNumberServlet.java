3a7943e561343814e426e99a0c8e6959910925e1
Destiney Rodney <destiney.rodney01@gmail.com>
2021-12-06 16:41:39 -0600

    feat:Partially complete guessnum

---
+++ src/main/java/GuessNumberServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Random;
+
+@WebServlet(name="GuessNumberServlet", urlPatterns ="/guess")
+public class GuessNumberServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Random random = new Random();
+        int randomNumber = random.nextInt(3) + 1;
+        System.out.println("random num" + randomNumber);
+
+
+    }
+
+}
---
+++ src/main/webapp/guess.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: destineyrodney
+  Date: 12/6/21
+  Time: 3:33 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Guess Number</title>
+    <%@include file="partials/head.jsp"%>
+</head>
+<body>
+<h1>Guess a number</h1>
+<form action="/guess" method="POST">
+    <label for="number">Number</label>
+    <input type="text" name="number" id="number" placeholder="Pick a number from 1-3">
+
+    <input type="submit">
+
+</form>
+
+</body>
+</html>
